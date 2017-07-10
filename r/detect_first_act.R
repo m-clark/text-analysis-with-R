1a3bf722942f5e26d98b1cf6450bab5a0bc6f5d0
Michael Clark <micl@umich.edu>
2017-07-10 16:53:19 -0400

    Start shakes processing rmd

--- r/detect_first_act.R
+++ r/detect_first_act.R
@@ -1,7 +1,7 @@
 # finds and cuts out everything up to and including the first instance of act, scene, or by
 
 detect_first_act <- function(text_tbl) {
-  act_lines = grep(text_tbl$text, pattern='^Act|^ACT|^by|^Sonnet')
+  act_lines = grep(text_tbl$text, pattern='^Act|^ACT|^by|^By|^Sonnet|^W\\. S\\.')
   if(length(act_lines) > 0) {
     idx = 1:act_lines[1]
     text_tbl = text_tbl %>% slice(-idx)
